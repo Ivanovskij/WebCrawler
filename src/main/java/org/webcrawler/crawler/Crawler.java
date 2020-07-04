7f6fe31854e1cf539a07e1463b40c51086244512
Ivanovskij <webworkk@mail.ru>
2020-07-04 18:50:58 +0300

    Move default crawl method to interface default implementation.

--- src/main/java/org/webcrawler/crawler/Crawler.java
+++ src/main/java/org/webcrawler/crawler/Crawler.java
@@ -1,11 +1,15 @@
 package org.webcrawler.crawler;
 
 import org.webcrawler.crawler.search.CrawlSearcher;
+import org.webcrawler.crawler.search.DefaultSearcher;
 import org.webcrawler.model.statistic.Statistic;
 
 import java.util.List;
 
 public interface Crawler {
     List<Statistic> crawl(String rootSeed, int depth, CrawlSearcher crawlSearcher);
-    List<Statistic> crawl(String rootSeed, int depth);
+
+    default List<Statistic> crawl(String rootSeed, int depth) {
+        return crawl(rootSeed, depth, new DefaultSearcher());
+    }
 }
