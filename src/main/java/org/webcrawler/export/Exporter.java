59f459910759c8f233e87c623cba662ec7eb4bb9
Ivanovskij <webworkk@mail.ru>
2020-07-06 21:32:34 +0300

    Add java doc to export api

--- src/main/java/org/webcrawler/export/Exporter.java
+++ src/main/java/org/webcrawler/export/Exporter.java
@@ -4,9 +4,24 @@ import org.webcrawler.model.statistic.Statistic;
 
 import java.util.List;
 
+/**
+ * General interface of different exports
+ */
 public interface Exporter {
 
+    /**
+     * Export all data in one file
+     * @param data - specified data to export
+     * @return true - export was successful, false - was not successful
+     */
     boolean exportAllInOne(List<Statistic> data);
+
+    /**
+     * Export data separately in a new file
+     * @param data - specified data to export
+     * @param numberOfPages - number of creating pages
+     * @return true - export was successful, false - was not successful
+     */
     boolean exportSeparately(List<Statistic> data, int numberOfPages);
 
 }
