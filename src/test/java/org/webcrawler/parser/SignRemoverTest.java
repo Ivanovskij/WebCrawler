0f6e63a1db134914295c99ee29ccc354eb0296f5
Ivanovskij <webworkk@mail.ru>
2020-07-06 19:06:56 +0300

    Cover parser package with help of unit tests

--- src/test/java/org/webcrawler/parser/SignRemoverTest.java
+++ src/test/java/org/webcrawler/parser/SignRemoverTest.java
@@ -6,19 +6,19 @@ import org.webcrawler.util.StringUtil;
 
 import static org.junit.Assert.assertEquals;
 
-public class HtmlRemoverTest {
+public class SignRemoverTest {
 
     private Remover remover;
 
     @Before
     public void setUp() {
-        remover = new HtmlRemover();
+        remover = new SignRemover();
     }
 
     @Test
-    public void shouldRemoveAllHtmlTags() {
+    public void shouldRemoveAllSigns() {
         String actual = StringUtil.splitByOneWhitespace(remover.remove(StringUtil.TEST_HTML_STRING));
-        String expected = StringUtil.splitByOneWhitespace("Page Title My First Heading My first paragraph. minsk");
+        String expected = StringUtil.splitByOneWhitespace("DOCTYPE html html head title Page Title title head body  h1 My First Heading h1 p My first paragraph p  a href https hello html minsk a body html");
         assertEquals(expected, actual);
     }
 
--- src/test/java/org/webcrawler/util/StringUtil.java
+++ src/test/java/org/webcrawler/util/StringUtil.java
@@ -5,11 +5,19 @@ public class StringUtil {
     public static final String SPACE_DELIMITER = " ";
     public static final String REMOVE_ALL_WHITESPACES_REGEX = "\\s+";
 
-    public static final String TEST_HTML_STRING = "<div class=\"dash weeks_dash\">\n" +
-            "\t\t\t\t\t\t<span class=\"dash_title\">nights</span>\n" +
-            "\t\t\t\t\t\t<div class=\"digit\">0</div>\n" +
-            "\t\t\t\t\t\t<div class=\"digit\">0</div>\n" +
-            "\t\t\t\t\t</div>";
+    public static final String TEST_HTML_STRING = "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "<head>\n" +
+            "<title>Page Title</title>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "\n" +
+            "<h1>My First Heading</h1>\n" +
+            "<p>My first paragraph.</p>\n" +
+            "\n" +
+            "<a href=\"https://hello.html\">minsk</a>" +
+            "</body>\n" +
+            "</html>\n";
 
     public static String splitByOneWhitespace(String text) {
         return text.trim().replaceAll(REMOVE_ALL_WHITESPACES_REGEX, SPACE_DELIMITER);
